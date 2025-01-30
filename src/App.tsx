66e6e17ec03c929c09c061ee6dda187d8e23c0eb
Thiago Silva <thiagoms98@outlook.com>
2025-01-30 14:10:35 -0300

    feat: create Header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import { Header } from "./components/Header"
 import styles from "./app.module.css"
 
 function App() {
+	function handleRestartGame() {
+		alert("Reiniciar o jogo")
+	}
 	return (
 		<div className={styles.container}>
 			<main>
-				<Header />
+				<Header current={5} max={10} onRestart={handleRestartGame} />
 			</main>
 		</div>
 	)
