d0284ebbd67424e1a67e377eda102bc62a2c4dad
Thiago Silva <thiagoms98@outlook.com>
2025-01-30 13:54:34 -0300

    first commit :)

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Header } from "./components/Header"
+import styles from "./app.module.css"
+
+function App() {
+	return (
+		<div className={styles.container}>
+			<main>
+				<Header />
+			</main>
+		</div>
+	)
+}
+
+export default App
