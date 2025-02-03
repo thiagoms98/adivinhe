4e5f640fd825eda08650e56dd8b421064e88cc56
Thiago Silva <thiagoms98@outlook.com>
2025-02-03 13:09:08 -0300

    feat: letters variations

--- src/components/LettersUsed/index.tsx
+++ src/components/LettersUsed/index.tsx
@@ -1,15 +1,16 @@
-import styles from './styles.module.css'
-import { Letter } from '../Letter'
+import styles from "./styles.module.css"
+import { Letter } from "../Letter"
 
-export function LettersUsed () {
-  return (
-    <div className={styles.lettersUsed}>
-      <h5>Letras utilizadas</h5>
+export function LettersUsed() {
+	return (
+		<div className={styles.lettersUsed}>
+			<h5>Letras utilizadas</h5>
 
-      <div>
-        <Letter value='R'/>
-        <Letter value='X'/>
-      </div>
-    </div>
-  )
+			<div>
+				<Letter value="R" size="small" color="correct" />
+				<Letter value="X" size="small" color="wrong" />
+				<Letter value="Z" size="small" />
+			</div>
+		</div>
+	)
 }
